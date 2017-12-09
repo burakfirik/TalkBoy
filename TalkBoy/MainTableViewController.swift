de595b65a758d659db6bb0194ca09444172a0b9a
Unknown <burakfirik@gmail.com>
2017-12-08 21:02:47 -0500

    added UITableViewController and AVAudio

---
+++ TalkBoy/MainTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  MainTableViewController.swift
+//  TalkBoy
+//
+//  Created by Burak Firik on 12/8/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+  var sounds: [Sound] =  []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  func getSound() {
+    if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+      if let sound = try? context.fetch(Sound.fetchRequest()) as? [Sound] {
+        if let soundData = sound {
+          sounds = soundData
+          tableView.reloadData()
+        }
+      }
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    getSound()
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return sounds.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    // Configure the cell...
+    let sound = sounds[indexPath.row]
+    if let soundRow = sound.name {
+      cell.textLabel?.text = soundRow
+    }
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        let sound = sounds[indexPath.row]
+        context.delete(sound)
+        getSound()
+      }
+    }
+  }
+  
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
---
+++ TalkBoy/RecordViewController.swift
@@ -0,0 +1,85 @@
+//
+//  RecordViewController.swift
+//  TalkBoy
+//
+//  Created by Burak Firik on 12/8/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordViewController: UIViewController {
+  
+  @IBOutlet weak var recordButton: UIButton!
+  @IBOutlet weak var playButton: UIButton!
+  @IBOutlet weak var soundNameTextField: UITextField!
+  @IBOutlet weak var addButton: UIButton!
+  var audioRecorder : AVAudioRecorder?
+  var audioPlayer : AVAudioPlayer?
+  var audioURL: URL?
+  
+  @IBAction func recordBtn(_ sender: Any) {
+    if let audioRecorder = self.audioRecorder {
+      if (audioRecorder.isRecording) {
+        audioRecorder.stop()
+        recordButton.setTitle("Record", for: .normal)
+        playButton.isEnabled = true
+        soundNameTextField.isEnabled = true
+        addButton.isEnabled = true
+      } else {
+        audioRecorder.record()
+        recordButton.setTitle("Stop", for: .normal)
+        playButton.isEnabled = false
+        soundNameTextField.isEnabled = false
+        addButton.isEnabled = false
+      }
+    }
+  }
+  
+  @IBAction func playBtn(_ sender: Any) {
+    if let audioURL = self.audioURL {
+      audioPlayer = try? AVAudioPlayer(contentsOf: audioURL)
+      audioPlayer?.play()
+    }
+    
+  }
+  
+  
+  @IBAction func addBtn(_ sender: Any) {
+    
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let session = AVAudioSession.sharedInstance()
+    try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+    try? session.overrideOutputAudioPort(.speaker)
+    try? session.setActive(true)
+    
+    if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+      let baseComponents = [basePath,"audio.m4a"]
+      if let audioURL = NSURL.fileURL(withPathComponents: baseComponents) {
+        var settings: [String: Any] = [:]
+        self.audioURL = audioURL
+        settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+        settings[AVSampleRateKey] = 44100.0
+        settings[AVNumberOfChannelsKey] = 2
+        audioRecorder = try? AVAudioRecorder(url: audioURL, settings: settings)
+        audioRecorder?.prepareToRecord()
+      }
+    }
+    playButton.isEnabled = false
+    soundNameTextField.isEnabled = false
+    addButton.isEnabled = false
+    
+    
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
